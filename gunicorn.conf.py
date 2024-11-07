0a396f4e2e00f8b03982defab7d25bf03244cc01
baidakovil <baidakovil@gmail.com>
2024-11-07 22:04:42 +0300

    try to disable custom logger for gunicorn

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -12,35 +12,35 @@ accesslog = "/var/log/baidakovru/gunicorn-access.log"
 errorlog = "/var/log/baidakovru/gunicorn-error.log"
 
 # Use the custom logger for Gunicorn
-logconfig_dict = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'root': {
-        'level': 'INFO',
-        'handlers': ['console', 'file'],
-    },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'generic',
-            'stream': 'ext://sys.stdout',
-        },
-        'file': {
-            'class': 'logging.handlers.RotatingFileHandler',
-            'formatter': 'generic',
-            'filename': '/var/log/baidakovru/app.log',
-            'maxBytes': 10240,
-            'backupCount': 5,
-        },
-    },
-    'formatters': {
-        'generic': {
-            'format': '%(asctime)s - %(name)-30s - %(levelname)-5s - %(filename)-25s:%(lineno)-5d - %(message)s',
-            'datefmt': '%Y-%m-%d %H:%M:%S',
-            'class': 'logging.Formatter',
-        },
-    },
-}
+# logconfig_dict = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'root': {
+#         'level': 'INFO',
+#         'handlers': ['console', 'file'],
+#     },
+#     'handlers': {
+#         'console': {
+#             'class': 'logging.StreamHandler',
+#             'formatter': 'generic',
+#             'stream': 'ext://sys.stdout',
+#         },
+#         'file': {
+#             'class': 'logging.handlers.RotatingFileHandler',
+#             'formatter': 'generic',
+#             'filename': '/var/log/baidakovru/app.log',
+#             'maxBytes': 10240,
+#             'backupCount': 5,
+#         },
+#     },
+#     'formatters': {
+#         'generic': {
+#             'format': '%(asctime)s - %(name)-30s - %(levelname)-5s - %(filename)-25s:%(lineno)-5d - %(message)s',
+#             'datefmt': '%Y-%m-%d %H:%M:%S',
+#             'class': 'logging.Formatter',
+#         },
+#     },
+# }
 
 
 def on_starting(server):
