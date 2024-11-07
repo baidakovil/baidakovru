b2bc1ae94dcdb00e33d276326d7a3b7ce88b1def
baidakovil <baidakovil@gmail.com>
2024-11-07 20:11:38 +0300

    add script to fast connect to server

---
+++ shscripts/connect_to_server_example.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ssh root@193.124.44.17
+
+# ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+# ssh-copy-id root@111.11.11.11
+# create file like this
+# chmod +x /home/path/connect_to_server.sh
+# alias connect_server='/home/path/connect_to_server.sh'
