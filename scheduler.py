e2d5ed80a3ac1833eec292d1b9e0b055e930c491
baidakovil <baidakovil@gmail.com>
2024-11-07 20:30:33 +0300

    add first run set up and interval setup via env

--- scheduler.py
+++ scheduler.py
@@ -1,18 +1,34 @@
 import logging
+import os
 import signal
 import sys
+from datetime import datetime, timedelta
 
 from apscheduler.schedulers.blocking import BlockingScheduler
+from dotenv import load_dotenv
 
 from pyscripts import log_config
 from pyscripts.update_data import update_all_services
 
+load_dotenv()
+SCHEDULER_INTERVAL_SEC = int(os.getenv('SCHEDULER_INTERVAL_SEC'))
+SCHEDULER_FIRST_RUN_SEC = int(os.getenv('SCHEDULER_FIRST_RUN_SEC'))
+
+
 logger = log_config.setup_logging()
 
 scheduler = BlockingScheduler()
-scheduler.add_job(update_all_services, 'interval', seconds=10)
+first_run_time = datetime.now() + timedelta(seconds=SCHEDULER_FIRST_RUN_SEC)
+scheduler.add_job(
+    update_all_services,
+    'interval',
+    seconds=SCHEDULER_INTERVAL_SEC,
+    next_run_time=first_run_time,
+)
 
-logger.info('Scheduler started')
+logger.info(
+    f'Scheduler started. First run in {SCHEDULER_FIRST_RUN_SEC} seconds, then every {SCHEDULER_INTERVAL_SEC} seconds.'
+)
 
 
 def sigterm_handler(signum, frame):
