bab6d6c0d35d8d3c247a64d9a5ac32b84b893b8f
baidakovil <baidakovil@gmail.com>
2025-01-23 22:13:07 +0300

    try to set core.FileMode false before pull

--- deploy.sh
+++ deploy.sh
@@ -35,6 +35,7 @@ else
     # Set ownership and permissions of application directory
     sudo chown -R www-data:www-data $APP_DIR
     sudo chmod -R 2775 $APP_DIR
+    sudo -u www-data git config core.fileMode false || { echo "Failed to set git permission ignoring"; exit 1; }
     cd $APP_DIR
     sudo -u www-data git pull origin main
 fi
