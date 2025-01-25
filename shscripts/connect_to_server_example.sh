e2785edd2136d08c67b1326cad33d9999b93f3d4
baidakovil <baidakovil@gmail.com>
2025-01-25 15:27:40 +0300

    Stop tracking and delete connect_to_server_example.sh from repository

--- shscripts/connect_to_server_example.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ssh root@193.124.44.17
-
-# ----------------------------------------
-# To manage this, do
-# chmod +x /home/path/connect_to_server.sh
-# alias c_s='/home/path/connect_to_server.sh'
