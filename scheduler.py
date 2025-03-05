dfb34bc9c2f35e63cdec04248d310b476dd2b2c8
baidakovil <baidakovil@gmail.com>
2025-03-05 22:15:24 +0300

    SCHEDULER CHANGES: added coalescing and misfire_grace_time, ThreadPoolExecutor and mypy satisfaction

--- scheduler.py
+++ scheduler.py
@@ -4,6 +4,7 @@ import signal
 import sys
 from datetime import datetime, timedelta
 
+from apscheduler.executors.pool import ThreadPoolExecutor
 from apscheduler.schedulers.blocking import BlockingScheduler
 from dotenv import load_dotenv
 
@@ -11,11 +12,25 @@ from pyscripts import log_config
 from pyscripts.update_data import update_all_platforms
 
 load_dotenv()
-SCHEDULER_INTERVAL_SEC = int(os.getenv('SCHEDULER_INTERVAL_SEC'))
-SCHEDULER_FIRST_RUN_SEC = int(os.getenv('SCHEDULER_FIRST_RUN_SEC'))
-
+# Default to 1 hour if not specified
+SCHEDULER_INTERVAL_SEC = int(os.getenv('SCHEDULER_INTERVAL_SEC', '3600'))
+# Default to 5 seconds if not specified
+SCHEDULER_FIRST_RUN_SEC = int(os.getenv('SCHEDULER_FIRST_RUN_SEC', '5'))
 
 logger = log_config.setup_logging()
+
+logger.info(
+    f'Using scheduler interval: {SCHEDULER_INTERVAL_SEC}s, '
+    f'first run delay: {SCHEDULER_FIRST_RUN_SEC}s'
+)
+
+# Configure executor and job defaults
+executors = {'default': ThreadPoolExecutor(max_workers=1)}
+job_defaults = {
+    'coalesce': True,
+    'max_instances': 1,
+    'misfire_grace_time': 300,  # 5 minutes grace time
+}
 
 scheduler = BlockingScheduler()
 first_run_time = datetime.now() + timedelta(seconds=SCHEDULER_FIRST_RUN_SEC)
